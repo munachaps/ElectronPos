ee46f086c46ae7ba623588674e50ce6c2316f7ee
itai chiriseri <itaineilchiriseri@gmail.com>
2023-10-04 15:32:18 +0200

    altered the product migration

--- ElectronPos/database/migrations/2023_09_29_073504_create_products_table.php
+++ ElectronPos/database/migrations/2023_09_29_073504_create_products_table.php
@@ -11,19 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {       
             $table->id();
             $table->string("name");
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('user_id');
             $table->string('barcode')->unique();
             $table->text('description')->nullable();
-            $table->decimal('price', 10, 2);
+            $table->decimal('price', 10, 2)->default(0.00); // Set an appropriate default value
             $table->boolean('status')->default(true);
-            $table->integer('quantity')->default(true);
-            $table->foreign('category_id')->references('id')->on('cattegories');
+            $table->integer('quantity')->default(0); // Set an appropriate default value
+            $table->foreign('category_id')->references('id')->on('categories'); // Corrected table name
             $table->foreign('user_id')->references('id')->on('users');
-            $table->timestamps();
+            $table->timestamps();            
         });
     }
 

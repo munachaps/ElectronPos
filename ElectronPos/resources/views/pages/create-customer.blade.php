6ad13b9d4c2f12c5c8bcc5fe8cf1b2b6e9731f85
itai chiriseri <inchiriseri@gmail.com>
2023-10-07 13:45:35 +0200

    changed the customer blade file to match customer related information

--- ElectronPos/resources/views/pages/create-customer.blade.php
+++ ElectronPos/resources/views/pages/create-customer.blade.php
@@ -83,10 +83,7 @@
                                 <label for="code">Code</label>
                                 <input type="text" name="code" id="code" class="form-control border border-2 p-2" required>
                             </div>
-                            <div class="form-group">
-                                <label for="user_id">User ID</label>
-                                <input type="text" name="user_id" id="user_id" class="form-control border border-2 p-2" required>
-                            </div>
+        
                             <div class="form-group">
                                 <label for="customer_taxnumber">Customer Tax Number</label>
                                 <input type="text" name="customer_taxnumber" id="customer_taxnumber" class="form-control border border-2 p-2" required>
@@ -101,11 +98,16 @@
                                 <label for="customer_address">Customer Address</label>
                                 <input type="text" name="customer_address" id="customer_address" class="form-control border border-2 p-2" required>
                             </div>
-
                             <div class="form-group">
                                 <label for="customer_phonenumber">Customer Phone Number</label>
                                 <input type="text" name="customer_phonenumber" id="customer_phonenumber" class="form-control border border-2 p-2" required>
                             </div>
+                            <div class="form-group">
+                            <label for="personal_care">Status</label>
+                            <select name="product_status" id="personal_care" class="form-control border border-2 p-2" required>
+                            <option value="active">Active</option>
+                            <option value="not_active">Not Active</option>
+                             </select>
                             <hr>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Create Customer</button>

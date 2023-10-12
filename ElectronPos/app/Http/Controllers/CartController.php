33b7e597288d34b9f5a10e66aeaa73a6a40dd9df
itai chiriseri <itaineilchiriseri@gmail.com>
2023-10-12 11:44:14 +0200

    altered the cart controller and added the store method

--- ElectronPos/app/Http/Controllers/CartController.php
+++ ElectronPos/app/Http/Controllers/CartController.php
@@ -22,8 +22,35 @@ class CartController extends Controller
 
     public function searchCartProduct(Request $request){
 
+        $request->validate([
+            'search_product' => 'required|exists:products,barcode',
+        ]);
+        
+        $barcode = $request->barcode;
 
-        dd($request->all());
+        $product = Product::where('barcode', $barcode)->first();
+        $cart = $request->user()->cart()->where('barcode', $barcode)->first();
+        if ($cart) {
+            // check product quantity
+            if ($product->quantity <= $cart->pivot->quantity) {
+                return response([
+                    'message' => 'Product available only: ' . $product->quantity,
+                ], 400);
+            }
+            // update only quantity
+            $cart->pivot->quantity = $cart->pivot->quantity + 1;
+            $cart->pivot->save();
+        } else {
+            if ($product->quantity < 1) {
+                return response([
+                    'message' => 'Product out of stock',
+                ], 400);
+            }
+            $request->user()->cart()->attach($product->id, ['quantity' => 1]);
+        }
+
+        return response('', 204);
+   
 
     }
 
